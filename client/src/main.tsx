b96ffdb37a5aaee183e38fae61fa89692080a4a1
DAVIDafergan <DAVIDAFERGAN999@gmail.com>
2026-01-20 16:57:47 +0200

    Ignore env file and sensitive data

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,15 @@
-import './index.css'; 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+--- START OF FILE client/src/main.tsx ---
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { AuthProvider } from './context/AuthContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider> {/* הוספנו את זה */}
+    <AuthProvider>
       <App />
     </AuthProvider>
   </React.StrictMode>,
-    <App />
-  </React.StrictMode>
-);
+)
