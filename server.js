55f6e875a4e5d1a639281aceaa538b6864828545
DAVIDafergan <DAVIDAFERGAN999@gmail.com>
2026-01-20 17:27:10 +0200

    Ignore env file and sensitive data

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
-
+app.set('trust proxy', 1);
 // --- הגדרות אבטחה ו-Middleware ---
 
 // הגדרת Helmet מותאמת אישית - פותר את שגיאות ה-CSP (המסך הלבן)
